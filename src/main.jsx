e4131c29efa518519dbbf52723cbd59d9e7e7340
Serhii Ozeran <ozeransm@gmail.com>
2023-11-12 22:56:58 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,11 @@ import { persistor, store } from './redux/store.js'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>    
-  // </React.StrictMode>,
+  </React.StrictMode>
 )
